3bbf4c55235bd10b48087ad4bd42972ce2403f80
d0p1 <contact@d0p1.eu>
2021-04-23 14:41:41 +0200

    CPinti: move exist.cpp into fs directory

--- OS2.1/CPinti/fs/exist.cpp
+++ OS2.1/CPinti/fs/exist.cpp
@@ -19,37 +19,30 @@
 		10/04/2020 - Correction state() - crash BT +0x6 (Ov3rl0w 09-AVRIL-2020)
 */
 
-#include <memory>
-#include <iostream>
 #include <sys/stat.h>
-#include "io.h"
-#include "CPinti.h" // Doevents
-#include "debug.h"
 
-namespace cpinti
+namespace cpinti::fs
 {
-	namespace file_manager
+	bool file_exist(const char *path)
 	{
-		bool file_exist(const char *path)
-		{
-			struct stat st;
+		struct stat st;
 
-			if (stat(path, &st) != 0)
-				return false;
-			if (S_ISREG(st.st_mode))
-				return true;
+		if (stat(path, &st) != 0)
 			return false;
-		}
-
-		bool dir_exist(const char *path)
-		{
-			struct stat st;
-
-			if (stat(path, &st) != 0)
-				return false;
-			if (S_ISDIR(st.st_mode))
-				return true;
-			return false;
-		}
+		if (S_ISREG(st.st_mode))
+			return true;
+		return false;
 	}
+
+	bool dir_exist(const char *path)
+	{
+		struct stat st;
+
+		if (stat(path, &st) != 0)
+			return false;
+		if (S_ISDIR(st.st_mode))
+			return true;
+		return false;
+	}
+
 }
